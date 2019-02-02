6ed898b432d5977aa7a4457822ca1e43a6cc61ed
Jing Cao <jingcao.yale@gmail.com>
2019-02-02 19:40:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,13 @@
 # Add  code here!
 def prime? (num)
-  if num == 1
+  if num == 2
     true
-  elsif num == 2
-    true
-  else
-      element = num - 1
-      if num % element == 0
-        return false
-      else
-        prime?(element)
-      end
-    end
   end
+
+  index = num
+  while index > 1 || index
+  end
+
 end
+
+prime?(2)
