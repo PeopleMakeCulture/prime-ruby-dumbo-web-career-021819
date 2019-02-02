473dc376f877e0d723e4cfbe820ccb950f673ee1
Jing Cao <jingcao.yale@gmail.com>
2019-02-02 19:23:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,13 +2,14 @@
 def prime? (num)
   if num == 1
     true
-  elif num == 2
-    true 
-  true
-  for index in (2..num-1) do
-    remainder = num % index
-    if remainder == 0
-      false
-    end
+  if num == 2
+    true
+  else
+    for index in (2..num-1) do
+      remainder = num % index
+      if remainder == 0
+        false
+      end
+    end 
   end
 end
