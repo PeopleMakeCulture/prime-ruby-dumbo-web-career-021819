a785ecff80cdfb822a7168dcb418d118c8271b9f
Jing Cao <jingcao.yale@gmail.com>
2019-02-02 20:35:01 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,17 +1,16 @@
 # Add  code here!
-def prime?(num)
- if num == 0 || num.abs == 1
-   return false
- elsif num == 2
-   return true
- else
-   for index in (2..(num-1).abs)
-     if (num % index) == 0
-       return false
-     else
-       return true
-     end
-   end
- end
- #return true
+def prime3?(num)
+  if num == 0 || num.abs == 1
+    return false
+  elsif num == 2
+    return true
+  else
+    for index in 2..num-1
+      puts index
+      if num % index == 0
+        return false
+      end
+    end
+    return true
+  end  
 end
